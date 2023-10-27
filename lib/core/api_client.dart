2a547b95c37115287827a0693dd7999fe7ab0ff9
haisyamm <haisyammaulana22@gmail.com>
2023-10-27 10:11:45 +0700

    first commit

---
+++ lib/core/api_client.dart
@@ -0,0 +1,84 @@
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  final Dio _dio = Dio();
+
+  Future<dynamic> registerUser(Map<String, dynamic>? data) async {
+    try {
+      Response response = await _dio.post(
+          'http://127.0.0.1:81/api/auth/register',
+          data: data,
+          // queryParameters: {'apikey': ApiSecret.apiKey},
+          // options: Options(headers: {'X-LoginRadius-Sott': ApiSecret.sott})
+          );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> login(String email, String password) async {
+    try {
+      Response response = await _dio.post(
+        'http://127.0.0.1:81/api/auth/login',
+        data: {
+          'email': email,
+          'password': password,
+        },
+        // queryParameters: {'apikey': ApiSecret.apiKey},
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> getUserProfileData(String accessToken) async {
+    try {
+      Response response = await _dio.get(
+        'http://127.0.0.1:81/api/auth/user-profile',
+        // queryParameters: {'apikey': ApiSecret.apiKey},
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> updateUserProfile({
+    required String accessToken,
+    required Map<String, dynamic> data,
+  }) async {
+    try {
+      Response response = await _dio.put(
+        'http://127.0.0.1:81/api/auth/user-profile',
+        data: data,
+        // queryParameters: {'apikey': ApiSecret.apiKey},
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+
+  Future<dynamic> logout(String accessToken) async {
+    try {
+      Response response = await _dio.get(
+        'http://127.0.0.1:81/api/auth/logout',
+        // queryParameters: {'apikey': ApiSecret.apiKey},
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+      return response.data;
+    } on DioError catch (e) {
+      return e.response!.data;
+    }
+  }
+}
